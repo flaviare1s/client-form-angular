edb958141af78430b3f9691d02ab271b7a9b8a1f
flaviare1s <flaviarster@gmail.com>
2025-07-10 16:37:10 -0300

    feat: customer-form.component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { CustomerFormComponent } from './pages/customer-form/customer-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: CustomerFormComponent }];
