ff5becf9e2808547557ca4d781832d3d422a387c
flaviare1s <flaviarster@gmail.com>
2025-07-11 20:02:13 -0300

    feat: json-server

--- src/app/pages/customer-list/customer-list.ts
+++ src/app/pages/customer-list/customer-list.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { CustomerService, Customer } from '../../services/customer';
 import { TableModule } from 'primeng/table';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 
 @Component({
@@ -9,21 +10,43 @@ import { ButtonModule } from 'primeng/button';
   standalone: true,
   templateUrl: './customer-list.html',
   styleUrls: ['./customer-list.scss'],
-  imports: [CommonModule, FormsModule, TableModule, ButtonModule],
+  imports: [CommonModule, TableModule, FormsModule, ButtonModule],
 })
-export class CustomerList {
+export class CustomerList implements OnInit {
+  customers: Customer[] = [];
+  totalRecords = 0;
+  loading = true;
+
   filterName = '';
   filterState = '';
-  customers = [];
-  limit = 10;
-  totalRecords = 0;
-  loading = false;
 
-  onFilterChange() {
-    // lógica de filtro
+  page = 1;
+  limit = 10;
+
+  constructor(private customerService: CustomerService) {}
+
+  ngOnInit(): void {
+    this.loadCustomers();
+  }
+
+  loadCustomers(): void {
+    this.loading = true;
+    this.customerService
+      .getCustomers(this.filterName, this.filterState, this.page, this.limit)
+      .subscribe((res) => {
+        this.customers = res.customers;
+        this.totalRecords = res.total;
+        this.loading = false;
+      });
   }
 
   onPageChange(event: any) {
-    // lógica de paginação
+    this.page = event.page + 1;
+    this.loadCustomers();
+  }
+
+  onFilterChange(): void {
+    this.page = 1;
+    this.loadCustomers();
   }
 }
